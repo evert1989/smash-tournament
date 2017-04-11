a291a726358efc9df386b3621a7d017617d6505f
Evert van der Horst <evert.vanderhorst@random.nu>
2017-04-11 23:07:30 +0200

    Added logic behind updating scores and going to the next round.

--- dev/js/app/view/page/round/round-page.js
+++ dev/js/app/view/page/round/round-page.js
@@ -1,11 +1,29 @@
 define([
+	// Vendors
+	'underscore',
+	// Collection
+	'collection/player-collection', // Singleton
+	// Component
+	'component/button',
+	// Models
+	'model/state/roster-state', // Singleton
 	// Views
 	'view/base/page-view',
+	'view/page/round/round-player',
 	// Template
 	'text!template/app/page/round/round-page.hbs'
 ], function (
+	// Vendors
+	_,
+	// Collection
+	PlayerCollection,
+	// Component
+	Button,
+	// Models
+	RosterState,
 	// Views
 	PageView,
+	RoundPlayer,
 	// Template
 	template
 ) {
@@ -16,6 +34,15 @@ define([
 
 		// Vars
 		// ----
+		// DOM
+		$playerContainer: {},
+		$number: {},
+
+		// Views
+		roundPlayers: [],
+
+		// Buttons
+		btnEndRound: {},
 
 
 		/** @constructor */
@@ -31,9 +58,16 @@ define([
 		 * @param {object} $parent
 		 */
 		start: function ($parent) {
-			if (this._super(template, $parent, null)) {
+			if (this._super(template, $parent, {round: RosterState.get('activeRound') + 1})) {
 				return;
 			}
+
+			this.$playerContainer = this.$('.player-container');
+			this.$number = this.$('.round-number-js');
+
+			this.createButtons();
+			this.populateRound();
+			this.addListeners();
 		},
 
 		stop: function () {
@@ -41,7 +75,95 @@ define([
 				return;
 			}
 
+			this.removeListeners();
 			this.$el.remove();
+		},
+
+
+		// Buttons
+		// -------
+		createButtons: function(){
+			this.btnEndRound = new Button();
+			this.btnEndRound.render(this.$('.btn-end-round-js'));
+		},
+
+
+		// Populating
+		// ----------
+		populateRound: function(){
+			this.clearRound();
+
+			_.each(RosterState.get('rounds')[RosterState.get('activeRound')], this.createSinglePlayer, this);
+		},
+
+		clearRound: function(){
+			_.invoke(this.roundPlayers, 'remove');
+			this.roundPlayers = [];
+		},
+
+
+		// Player
+		// ------
+		createSinglePlayer: function(playerName){
+			let targetPlayer = PlayerCollection.findWhere({name: playerName});
+			let player = new RoundPlayer({model: targetPlayer});
+			player.render(this.$playerContainer);
+
+			this.roundPlayers.push(player);
+		},
+
+		// Helpers
+		// -------
+		allValuesAreSet: function(){
+			let isValueSet = true;
+
+			_.each(this.players, function(playerView){
+				if(!isValueSet) return;
+
+				isValueSet = playerView.$score.val().length > 0;
+			}, this);
+
+			return isValueSet;
+		},
+
+
+		// Events
+		// ------
+		onChangeActiveRound: function(){
+			this.$number.text(RosterState.get('activeRound') + 1);
+			this.populateRound();
+		},
+
+		onClickEndRound: function(){
+			if(!this.allValuesAreSet()) {
+				return;
+			}
+
+			_.invoke(this.roundPlayers, 'updateScore');
+
+			let activeRound = RosterState.get('activeRound');
+			activeRound += 1;
+
+			if(activeRound > RosterState.get('totalRounds')){
+				// todo: go to semi finales
+
+			} else {
+				RosterState.set({activeRound: activeRound});
+			}
+
+		},
+
+
+		// Listeners
+		// ---------
+		addListeners: function(){
+			this.listenTo(RosterState, 'change:activeRound', this.onChangeActiveRound);
+			this.listenTo(this.btnEndRound, 'click', this.onClickEndRound);
+		},
+
+		removeListeners: function(){
+			this.stopListening(RosterState, 'change:activeRound', this.onChangeActiveRound);
+			this.stopListening(this.btnEndRound, 'click', this.onClickEndRound);
 		}
 	});
 });
---
+++ dev/js/app/view/page/round/round-player.js
@@ -0,0 +1,53 @@
+define([
+	// Collection
+	'collection/player-collection',
+	// Views
+	'view/base/base-view',
+	// Templates
+	'text!template/app/page/round/round-player.hbs'
+], function (
+	// Collection
+	PlayerCollection,
+	// Views
+	BaseView,
+	// Template
+	template
+) {
+
+	'use strict';
+
+	return BaseView.extend({
+
+		// Vars
+		// ----
+		model: null,
+
+		// DOM
+		$score: {},
+
+
+		/** @constructor */
+		initialize: function (options) {
+			this._super(options);
+		},
+
+
+		// Template
+		// --------
+		render: function($parent){
+			this._super(template, $parent, this.model.toJSON());
+
+			this.$score = this.$('.score-js');
+		},
+
+
+		// Update
+		// ------
+		updateScore: function(){
+			let points = this.model.get('points');
+			points += parseInt(this.$score.val());
+
+			this.model.set({points: points});
+		}
+	});
+});
